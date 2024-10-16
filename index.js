d2a2490924fb2548e64d4cf819fe6bf287bca514
howabsurd <poptanikrish1@gmail.com>
2024-10-16 20:45:48 +0530

    Database Config

--- index.js
+++ index.js
@@ -2,19 +2,41 @@ const express = require("express");
 require("dotenv").config();
 const router = require("./routes/index");  // Correct route import
 const errorMiddleware = require("./middlewares/Error")
+const { Sequelize } = require('sequelize');
 
 const app = express();
 
 // Middleware (if any) can go here
+app.use((req, res, next) => {
+  console.log("Req Path:", req.path);
+  console.log("Req Method: ", req.method)
+  next();
+});
 
 app.use(express.json());  // Example of middleware for parsing JSON
 app.use("/api", router)
 
+const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
+  host: process.env.DATABASE_HOST,
+  dialect: process.env.DATABASE_DIALECT,
+});
 
-const PORT = process.env.PORT || 3000;  // Fallback if .env is not set
+// Test database connection
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+
+const PORT = process.env.PORT || 3000; 
 
 app.listen(PORT, () => {
   console.log(`App is listening on ${PORT}`);
 });
+
+
 
 app.use(errorMiddleware)
